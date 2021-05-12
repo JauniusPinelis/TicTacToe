fdb445e47cabf4c7c5543cc110d44fb5bcd9e8c4
JauniusPinelis <JauniusPinelis@gmail.com>
2021-05-12 19:05:08 +0300

    WinningCombinations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function(event) {
-    var target = event.target
+    var target = event.target;
     markX(target);
 });
 
@@ -17,6 +17,22 @@ function markX(element){
     else {
         // Do nothing
     }
+}
+
+function anyMovesLeft(){
+    var cells = Array.from(document.getElementsByClassName("cell"));
+
+    cells = cells.filter(cell => cell.innerText == "");
+
+    if (cells.length == 0){
+        //modify .game--status to be draw
+        var gameStatusElement = document.querySelector(".game--status");
+
+        gameStatusElement.innerText = "DRAW";
+        return false; //the game is finished
+    }
+
+    return true; // the game continues
 }
 
 function opponentMove(){
@@ -50,6 +66,7 @@ function checkResult(){
     return anyMovesLeft();
 }
 
+
 function isWinnerDetermined(){
     let winningCellCombinations = [
         [0,1,2],
@@ -61,28 +78,35 @@ function isWinnerDetermined(){
         [0,4,8],
         [2,4,6]
     ]
-        //Go  through there winning combinatations
-        //Get all cells of those attribute values
-        // Check all the elements have the same inner text (X or 0)
-        // Carefull of empty values
+        //Go  through there winning combinations
+        winningCellCombinations.forEach(combination =>
+            {
+                let i1 = combination[0];
+                let i2 = combination[1];
+                let i3 = combination[2];
 
-       // 2. We have 3 X or 3 O in a row, column or sideways
+                let element1 = document.querySelector(`[data-cell-index="${i1}"]`);
+                let element2 = document.querySelector(`[data-cell-index="${i2}"]`);
+                let element3 = document.querySelector(`[data-cell-index="${i3}"]`);
+
+                let isGameCompleted = 
+                    element1.innerText == element2.innerText 
+                    && element2.innerText == element3.innerText
+                    && element1.innerText != "";
+                
+                if (isGameCompleted){
+                    var gameStatusElement = document.querySelector(".game--status");
+
+                    gameStatusElement.innerText = `${element1.innerText} has won the game`
+                    return true;
+                }
+            });
+
+        return false;
+        
 }
 
-function anyMovesLeft(){
-       
-    var cells = Array.from(document.getElementsByClassName("cell"));
 
-    cells = cells.filter(cell => cell.innerText == "");
-
-    if (cells.length == 0){
-        //modify .game--status to be draw
-        document.querySelector(".game--status").innerText = "DRAW";
-        return false; //the game is finished
-    }
-
-    return true; // the game continues
-}
 
 function resetGame(){
 
